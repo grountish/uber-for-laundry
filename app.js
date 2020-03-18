0b488630e87df0a69b619dcd1ae65bc9ea6d4456
grountish <grountish@gmail.com>
2020-03-18 22:59:44 +0100

    done faltan algunos estilos

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 require('dotenv').config();
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const bodyParser = require('body-parser');
 const favicon = require('serve-favicon');
 const hbs = require('hbs');
 const mongoose = require('mongoose');
+const app = express();
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
 
 mongoose
   .connect('mongodb://localhost/uber-for-loundry', {
@@ -25,16 +28,18 @@ mongoose
     console.error('Error connecting to mongo', err);
   });
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+
+const authRoutes = require('./routes/auth');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const laundryRoutes = require('./routes/laundry');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// Middleware Setup
+// Middleware Setup--todos los app.use son middlewares!!
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -47,8 +52,35 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// has to be before routes declaration
+app.use(session({
+  secret: 'never do your own laundry again',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
+
+app.use((req, res, next) => {
+  if (req.session.currentUser) {
+    res.locals.currentUserInfo = req.session.currentUser;
+    res.locals.isUserLoggedIn = true;
+  } else {
+    res.locals.isUserLoggedIn = false;
+  }
+
+  next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRoutes);
+app.use('/laundry', laundryRoutes);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
